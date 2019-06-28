94bbf89031b3b9850196ded7919042ba2dfb5efc
Davide Calabrò <10566859@polimi.it>
2019-06-28 17:39:22 +0200

    First read from stdin

--- main.c
+++ main.c
@@ -1,7 +1,30 @@
 #include<stdio.h>
+#include<string.h>
+
+#define N 100
+#define STRING_EQUALS 0
+#define COMMAND_LENGTH 6
+
+void read();
 
 int main(int argc, char** argv){
-   printf("Hello world!\n");
+   read();
 
    return 0;
+}
+
+void read(){
+   int i = 0;
+   char c;
+   char command[COMMAND_LENGTH+1];
+
+   do{
+      scanf("%s ", command);
+
+      if(!strcmp(command, "addrel"))
+	 printf("addrel\n");
+      else if(!strcmp(command, "addent"))
+	 printf("addent\n");
+
+   }while(!strcmp(command, "end"));
 }
